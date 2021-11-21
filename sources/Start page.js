b6a8abd1f82de37de191e1896983f9f0ff3467a9
Lemmy00 <64690721+Lemmy00@users.noreply.github.com>
2021-11-21 07:02:10 +0100

    Organizing files

---
+++ sources/Start page.js
@@ -0,0 +1,22 @@
+"use strict"
+
+const music=new Audio();
+music.src="../data/Music.mp3";
+music.loop=true;
+
+const button=document.querySelector(".music");
+button.addEventListener("click", playMusic);
+
+function playMusic(event){
+    if(button.textContent==="Play Music"){
+        music.play();
+        button.textContent="Stop Music";
+    }
+    else{
+        music.pause();
+        button.textContent="Play Music";
+    }
+}
+
+
+
